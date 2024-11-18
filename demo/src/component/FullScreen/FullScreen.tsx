85c663d4e019e39e1b85ae6f7f2943059c1f3361
designDefined <designdefined.by@gmail.com>
2024-11-18 18:57:27 +0900

    feat: Update demo to new flexive/core

--- demo/src/component/FullScreen/FullScreen.tsx
+++ demo/src/component/FullScreen/FullScreen.tsx
@@ -1,19 +1,5 @@
-import { Article, FlexiveArticleProps, useFlexiveSystem } from "@flexive/core";
+import { Article, PropsWithoutRef } from "@flexive/core";
 
-type PageProps = FlexiveArticleProps;
+type FullScreenProps = PropsWithoutRef<"article">;
 
-export function FullScreen({ f, className, children }: PageProps) {
-  const { fx, cx } = useFlexiveSystem(true, { f, className });
-
-  return (
-    <Article
-      className={cx.root()}
-      f={fx.root({
-        align: ["baseline", "hidden", "100vw", "100vw"],
-        justify: ["start", "auto", "100vh", "100vh"],
-      })}
-    >
-      {children}
-    </Article>
-  );
-}
+export const FullScreen = (props: FullScreenProps) => <Article p={24} {...props} />;
--- demo/src/ui/home/HomePage.tsx
+++ demo/src/ui/home/HomePage.tsx
@@ -1,39 +1,13 @@
 import styles from "./HomePage.module.css";
-import { bindCSS, Div, H1, H4, Li, Nav, Ol, Section } from "@flexive/core";
 import { FullScreen } from "../../component/FullScreen/FullScreen";
-import { ConnceptSection } from "./ConceptSection/ConceptSection";
+import { bindCSS, H1 } from "@flexive/core";
 
 const cx = bindCSS(styles);
 
-export function HomePage() {
+export const HomePage = () => {
   return (
-    <FullScreen className={cx("HomePage")}>
-      <Section
-        f={{
-          spacing: [40, 180],
-          justify: ["center", "hidden", "100vh"],
-          align: ["center", "hidden", "100vw"],
-        }}
-      >
-        <Div f={{ flow: ["column", "nowrap", "center"] }}>
-          <H1 className={cx("title")}>Flexive</H1>
-          <H4>Flexbox-based design system builder for ReactJS</H4>
-        </Div>
-        <Nav>
-          <Ol f={{ flow: ["row"], spacing: [0, 24] }}>
-            <Li f={{ flow: ["column", "nowrap", "center", "center"], spacing: [[12, 32]] }} className={cx("tab")}>
-              Getting Started
-            </Li>
-            <Li f={{ flow: ["column", "nowrap", "center", "center"], spacing: [[12, 32]] }} className={cx("tab")}>
-              Learn Flexbox
-            </Li>
-            <Li f={{ flow: ["column", "nowrap", "center", "center"], spacing: [[12, 32]] }} className={cx("tab")}>
-              Design Systems
-            </Li>
-          </Ol>
-        </Nav>
-      </Section>
-      <ConnceptSection />
+    <FullScreen className={cx("HomePage")} style={{ background: "red" }}>
+      <H1 className={cx("title")}>Flexive!</H1>
     </FullScreen>
   );
-}
+};
