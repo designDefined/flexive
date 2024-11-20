4e6ad7eec0f7734fd687458a6df95e4a75913704
designDefined <designdefined.by@gmail.com>
2024-11-20 10:42:15 +0900

    feat: Test for utility styles

--- demo/src/ui/home/HomePage.tsx
+++ demo/src/ui/home/HomePage.tsx
@@ -6,8 +6,10 @@ const cx = bindCSS(styles);
 
 export const HomePage = () => {
   return (
-    <FullScreen className={cx("HomePage")} py={1} pb={3} style={{ background: "red" }}>
-      <H1 className={cx("title")}>Flexive!</H1>
+    <FullScreen className={cx("HomePage")} py={1} pb={3} style={{ background: "red" }} rad={20}>
+      <H1 className={cx("title")} absolute right={40} top={200}>
+        Flexive!
+      </H1>
       <Div px={2} py={4} alignSelfC="start" over overM="scroll" hideC>
         Test
       </Div>
