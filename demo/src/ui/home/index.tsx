ea83ef3e347ef5ba49584b95b7df24c132f11700
designDefined <designdefined.by@gmail.com>
2025-01-15 11:40:11 +0900

    feat: Simple test for gaps

--- demo/src/ui/home/index.tsx
+++ demo/src/ui/home/index.tsx
@@ -1,5 +1,5 @@
 import styles from "./index.module.css";
-import { bindCSS, H1, Main } from "@flexive/core";
+import { bindCSS, Div, H1, Main } from "@flexive/core";
 
 const cx = bindCSS(styles);
 
@@ -7,6 +7,15 @@ export const HomePage = () => {
   return (
     <Main className={cx("HomePage")}>
       <H1 className={cx("title")}>Flexive</H1>
+      <Div sizeM={500} wrap alignC="start" g={200} gM={100}>
+        {[1, 2, 3, 4, 5, 6, 7, 8].map(val => {
+          return (
+            <Div key={val} sizeM={80} sizeC={80} alignC alignM style={{ border: "1px solid black" }}>
+              {val}
+            </Div>
+          );
+        })}
+      </Div>
     </Main>
   );
 };
