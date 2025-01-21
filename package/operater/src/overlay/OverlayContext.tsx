e6aaf1987d3f137857d1d1a0cd1277b25f142c5e
designDefined <designdefined.by@gmail.com>
2025-01-21 15:12:57 +0900

    @flexive/operator/0.3.3 (#31)
    * feat: Add 'isClosing' state to overlay context
    
    * release: Bump version

--- package/operater/src/overlay/OverlayContext.tsx
+++ package/operater/src/overlay/OverlayContext.tsx
@@ -1,16 +1,30 @@
 import { createContext, PropsWithChildren, useContext } from "react";
 
 type CloseFunctions = { close: () => void; closeAfter: (ms: number) => void };
-type OverlayContextValue = { stack: CloseFunctions[]; close: () => void; closeAfter: (ms: number) => void };
+type OverlayContextValue = {
+  stack: CloseFunctions[];
+  close: () => void;
+  closeAfter: (ms: number) => void;
+  isClosing: boolean;
+};
 
-export const OverlayContext = createContext<OverlayContextValue>({ stack: [], close: () => {}, closeAfter: () => {} });
+export const OverlayContext = createContext<OverlayContextValue>({
+  stack: [],
+  close: () => {},
+  closeAfter: () => {},
+  isClosing: false,
+});
 
-type OverlayContextProviderProps = PropsWithChildren & { close: () => void; closeAfter: (ms: number) => void };
+type OverlayContextProviderProps = PropsWithChildren & {
+  close: () => void;
+  closeAfter: (ms: number) => void;
+  isClosing: boolean;
+};
 
-export const OverlayContextProvider = ({ close, closeAfter, children }: OverlayContextProviderProps) => {
+export const OverlayContextProvider = ({ close, closeAfter, isClosing, children }: OverlayContextProviderProps) => {
   const { stack } = useContext(OverlayContext);
   return (
-    <OverlayContext.Provider value={{ stack: [...stack, { close, closeAfter }], close, closeAfter }}>
+    <OverlayContext.Provider value={{ stack: [...stack, { close, closeAfter }], close, closeAfter, isClosing }}>
       {children}
     </OverlayContext.Provider>
   );
