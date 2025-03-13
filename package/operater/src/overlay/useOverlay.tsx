fc06ab642514905b5504d21a33dc5fd12459dc7e
designDefined <designdefined.by@gmail.com>
2025-03-13 11:57:39 +0900

    feat: Add lifecycle event handlers to useOverlay"

--- package/operater/src/overlay/useOverlay.tsx
+++ package/operater/src/overlay/useOverlay.tsx
@@ -4,7 +4,12 @@ import { useDelay } from "../temporal";
 
 export type OverlayRenderer = (children: ReactNode, container: Element | DocumentFragment) => ReactPortal;
 
-type OverlayOption = { at?: Element | DocumentFragment };
+type OverlayOption<Context> = {
+  at?: Element | DocumentFragment;
+  onOpen?: (context: Context) => void;
+  onClose?: (context: Context) => void;
+  onUpdate?: (context: Context, prevContext: Context) => void;
+};
 
 type OverlayState<Context> = { isOpen: false; context?: undefined } | { isOpen: true; context: Context };
 
@@ -15,12 +20,25 @@ type OverlayAction<Context> =
 
 type OverlayReducer<Context> = (state: OverlayState<Context>, action: OverlayAction<Context>) => OverlayState<Context>;
 
-export const useOverlay = <Context = undefined,>(renderer: OverlayRenderer, { at }: OverlayOption = {}) => {
+export const useOverlay = <Context = undefined,>(
+  renderer: OverlayRenderer,
+  { at, onOpen, onClose, onUpdate }: OverlayOption<Context> = {},
+) => {
   const { delay, isDelaying, duration } = useDelay();
   const [{ isOpen, context }, dispatch] = useReducer<OverlayReducer<Context>>(
     (prev, [code, context]) => {
-      if (code === 2) return prev.isOpen ? { ...prev, context } : prev;
-      if (code === 1) return { isOpen: false };
+      if (code === 2) {
+        if (!prev.isOpen) return prev;
+        onUpdate?.(context, prev.context);
+        return { ...prev, context };
+      }
+      if (code === 1) {
+        if (!prev.isOpen) return prev;
+        onClose?.(prev.context);
+        return { isOpen: false };
+      }
+      if (prev.isOpen) return prev;
+      onOpen?.(context);
       return { isOpen: true, context };
     },
     { isOpen: false },
